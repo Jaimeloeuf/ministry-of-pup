6b1af5c9f6cc90b94c3976768fab4115d558015e
Jaime <jaimeloeuf@gmail.com>
2021-10-10 02:06:37 +0800

    Refactor cancel appointment view to app:     Refactor the cancel appointment view and logic into its own standalone     app that will be loaded asynchronously if the URL is for cancelling     appointment. So that the app is not loaded most of the time when users     just use the app to book appointments.

--- booking/src/cancel.js
+++ booking/src/cancel.js
@@ -1,21 +1,79 @@
-import { text, h1, b, br, div, label } from "@hyperapp/html";
+import { app } from "hyperapp";
+import { main, text, h1, b, br, div, label } from "@hyperapp/html";
+import { oof } from "simpler-fetch";
 
-const view = ({ appointmentID }) =>
-  div({ class: "px-5 pt-5", style: { "max-width": "30em" } }, [
-    div({ class: "columns is-multiline is-mobile" }, [
-      div(
-        { class: "column is-full" },
-        h1(
-          { class: "title is-4 has-text-danger" },
-          text("Your appointment has been cancelled!")
-        )
+import loaderView from "./loader.js";
+
+// Effect to call the cancel appointment API and update state once cancelled
+// Have a recaptcha layer to prevent spam/bots
+const cancelAppointment = async (dispatch, appointmentID) =>
+  grecaptcha.ready(function () {
+    grecaptcha
+      .execute("6Lcex6QcAAAAADus4RtnoqwskQoXcB2DwgCav11Z", {
+        action: "cancelAppointment",
+      })
+      .then(
+        // Main API call logic of the cancel appointment Effect
+        async function _cancelAppointment(token) {
+          const response = await oof
+            .POST(`/appointment/cancel/${appointmentID}?token=${token}`)
+            .run()
+            .then((response) => response.json());
+
+          // Maybe set something in state instead to show a retry UI or smth
+          if (!response.ok) throw new Error(response.error);
+
+          // Anonymous function action
+          dispatch((state) => ({ ...state, loader: false }));
+        }
+      );
+  });
+
+export default (appointmentID) =>
+  app({
+    init: [
+      { appointmentID, loader: true },
+
+      // Run `loadDates` effect on init
+      [cancelAppointment, appointmentID],
+    ],
+
+    view: ({ loader, appointmentID }) =>
+      main(
+        {
+          style: {
+            width: "86vw",
+            "max-width": "40em",
+
+            // CSS to make entire app centered vertically
+            // @todo Although this makes the available x axis smaller esp for booking view
+            cssText:
+              "display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: left; height: 100vh;",
+          },
+        },
+        loader
+          ? loaderView
+          : div({ class: "px-5 pt-5", style: { "max-width": "30em" } }, [
+              div({ class: "columns is-multiline is-mobile" }, [
+                div(
+                  { class: "column is-full" },
+                  h1(
+                    { class: "title is-4 has-text-danger" },
+                    text("Your appointment has been cancelled!")
+                  )
+                ),
+
+                div(
+                  { class: "column is-full" },
+                  label([
+                    b(text("Appointment ID")),
+                    br(),
+                    text(`${appointmentID}`),
+                  ])
+                ),
+              ]),
+            ])
       ),
 
-      div(
-        { class: "column is-full" },
-        label([b(text("Appointment ID")), br(), text(`${appointmentID}`)])
-      ),
-    ]),
-  ]);
-
-export default view;
+    node: document.getElementById("app"),
+  });
--- booking/src/main.js
+++ booking/src/main.js
@@ -14,98 +14,110 @@ oof.baseUrl(
 import bookingView from "./booking.js";
 import detailsView from "./details.js";
 import completeView from "./complete.js";
-import cancelView from "./cancel.js";
 import notFoundView from "./notFound.js";
 import loaderView from "./loader.js";
 
 import { loadDates } from "./loadDates.js";
 
-app({
-  init: [
-    {
-      // Route is just used to show the different views
-      // It does not actually reflect the URL path of the app
-      route: "/",
+/*
+  Checks if the URL is a hash based URL requesting to cancel an appointment.
+  If true, parse out the appointmentID, load the cancelAppointmentApp asynchronously and run the app.
+  Else, run the standard booking app.
+  
+  All the views for the standard booking app is loaded directly,
+  because most of the time users would be using this app to book.
+  And to optimise for the most common path, the booking app is
+  loaded optimistically to prevent asynchronously loading another
+  JS file to run the actual main booking app.
+  Only the cancel appointment app is loaded asynchronously as it
+  is not the most common use case and is allowed to take more time.
+*/
 
-      cancellingAppointment: (function () {
-        // Match for the pattern `/#/cancel/$appointmentID`
-        const match = location.hash.match(/#\/cancel\/(\w+)/);
+// Match for the pattern `/#/cancel/$appointmentID`
+const match = location.hash.match(/#\/cancel\/(\w+)/);
 
-        // Get the second element which is the appointmentID if there is a match
-        return match !== null && match[1];
-      })(),
+// Get the second element which is the appointmentID if there is a match
+const appointmentID = match !== null && match[1];
 
-      loader: false,
-
-      dog: { name: "French bull dog 1" },
-
-      datesAvailable: [],
-      selectedDate: undefined,
-
-      details: {
-        fname: undefined,
-        lname: undefined,
-        number: undefined,
-        email: undefined,
-      },
-
-      // Set after appointment is booked, where this is returned from booking API
-      appointmentID: undefined,
-    },
-
-    // Run `loadDates` effect on init
-    [loadDates],
-  ],
-
-  view: ({
-    route,
-    cancellingAppointment,
-    loader,
-    dog,
-    datesAvailable,
-    selectedDate,
-    details,
-    appointmentID,
-  }) =>
-    main(
+if (appointmentID)
+  import("./cancel.js").then(({ default: cancelAppointmentApp }) =>
+    cancelAppointmentApp(appointmentID)
+  );
+else
+  app({
+    init: [
       {
-        style: {
-          width: "86vw",
-          "max-width": "40em",
+        // Route is just used to show the different views
+        // It does not actually reflect the URL path of the app
+        route: "/",
 
-          // CSS to make entire app centered vertically
-          // @todo Although this makes the available x axis smaller esp for booking view
-          cssText:
-            "display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: left; height: 100vh;",
+        loader: false,
+
+        dog: { name: "French bull dog 1" },
+
+        datesAvailable: [],
+        selectedDate: undefined,
+
+        details: {
+          fname: undefined,
+          lname: undefined,
+          number: undefined,
+          email: undefined,
         },
+
+        // Set after appointment is booked, where this is returned from booking API
+        appointmentID: undefined,
       },
-      [
-        loader
-          ? loaderView
-          : (function () {
-              if (cancellingAppointment)
-                return cancelView({ appointmentID: cancellingAppointment });
 
-              switch (route) {
-                case "/":
-                  return bookingView({ dog, datesAvailable });
-                case "/details":
-                  return detailsView;
-                case "/complete":
-                  return completeView({
-                    dog,
-                    selectedDate,
-                    details,
-                    appointmentID,
-                  });
+      // Run `loadDates` effect on init
+      [loadDates],
+    ],
 
-                default:
-                  // @todo Load it asynchronously   return import("./notFound.js");
-                  return notFoundView;
-              }
-            })(),
-      ]
-    ),
+    view: ({
+      route,
+      loader,
+      dog,
+      datesAvailable,
+      selectedDate,
+      details,
+      appointmentID,
+    }) =>
+      main(
+        {
+          style: {
+            width: "86vw",
+            "max-width": "40em",
 
-  node: document.getElementById("app"),
-});
+            // CSS to make entire app centered vertically
+            // @todo Although this makes the available x axis smaller esp for booking view
+            cssText:
+              "display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: left; height: 100vh;",
+          },
+        },
+        [
+          loader
+            ? loaderView
+            : (function () {
+                switch (route) {
+                  case "/":
+                    return bookingView({ dog, datesAvailable });
+                  case "/details":
+                    return detailsView;
+                  case "/complete":
+                    return completeView({
+                      dog,
+                      selectedDate,
+                      details,
+                      appointmentID,
+                    });
+
+                  default:
+                    // @todo Load it asynchronously   return import("./notFound.js");
+                    return notFoundView;
+                }
+              })(),
+        ]
+      ),
+
+    node: document.getElementById("app"),
+  });
