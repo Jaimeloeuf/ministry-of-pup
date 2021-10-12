245367320742e80b5f2c1401097804dbdcd5a36b
Jaime <jaimeloeuf@gmail.com>
2021-10-12 14:00:01 +0800

    Create firebase auth header generator:     Create a function to generate and return an auth header object using     firebase auth's JWT.

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -16,5 +16,16 @@ const auth = getAuth(firebaseApp);
 // Make firebase auth use browser's default language
 auth.useDeviceLanguage();
 
+/**
+ * Only returns authentication header object if user is authenticated.
+ * If user is unauthenticated, this does not throw and just returns undefined.
+ * @function getAuthHeader
+ * @returns {object | undefined} Authentication header object or nothing.
+ */
+async function getAuthHeader() {
+  if (auth.currentUser)
+    return { Authorization: `Bearer ${await auth.currentUser.getIdToken()}` };
+}
+
 // Export only the items that will be used
-export { auth, onAuthStateChanged };
+export { auth, onAuthStateChanged, getAuthHeader };
