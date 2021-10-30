71464a3e5d1c91e9f583bfa935ed2dfc8b07e032
Jaime <jaimeloeuf@gmail.com>
2021-10-30 18:30:11 +0800

    Fix newsletter API: 1.  Fix email template to use HTML and embed the unsub link. 2.  Update the subscribe API to handle cases where the user was once     subscribed but decided to unsub.

--- api/src/routes/newsletter.js
+++ api/src/routes/newsletter.js
@@ -13,18 +13,25 @@ const verifyRecaptcha = require("../utils/verifyRecaptcha");
 const unixseconds = require("unixseconds");
 const { asyncWrap } = require("express-error-middlewares");
 
-const emailString = (newsletterDocID) =>
-  `Hello,
-
+const emailString = (newsletterDocID) => `
+Hello,
+<br />
+<br />
 Thank you for subscribing to our newsletter, our Puppy Minister will be sending important updates on puppies every now and then!
-
------
-
+<br />
+<br />
+<hr />
+<br />
 Contact Details:
+<br />
 Whatsapp us through https://wa.me/6588022177
+<br />
 Email us at ministryofpup@gmail.com
-Or call us at 8802,2177 daily between 11am - 8pm for help
-Unsubscribe https://ministryofpup.com/#/unsubscribe/${newsletterDocID}`;
+<br />
+Or call us at 8802,2177 daily between 11am - 8pm for help.
+<br />
+<br />
+Unsubscribe <a href="https://ministryofpup.com/#/unsubscribe/${newsletterDocID}">here</a>`;
 
 /**
  * Save subscription, assuming src is LP (Landing Page) and send them a email to confirm it
@@ -33,24 +40,45 @@ Unsubscribe https://ministryofpup.com/#/unsubscribe/${newsletterDocID}`;
  * @name POST /newsletter/subscribe
  * @returns Sucess indicator
  */
-router.post(
-  "/subscribe",
-  verifyRecaptcha,
-  express.json(),
-  asyncWrap(async (req, res) =>
-    fs
+router.post("/subscribe", verifyRecaptcha, express.json(), async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const snapshot = await fs
       .collection("newsletter")
-      .add({ email, src: "LP", time: unixseconds() })
-      .then(({ id }) => ({
-        to: req.body.email,
-        from: process.env.notificationEmailSender,
-        subject: `Ministry Of Pup: You are now Subscribed to cuteness!`,
-        text: emailString(id),
-      }))
-      .then(sendMail.send)
-      .then(() => res.status(200).json({ ok: true }))
-  )
-);
+      .where("email", "==", email)
+      .get();
+
+    // If the snapshot is empty, return false to specify user is not subscribed
+    // Else if user has an account but the subscription was previously cancelled, resubscribe
+    // Else assume only 1 document for that user, and return the first user document data
+    if (snapshot.empty)
+      await fs
+        .collection("newsletter")
+        .add({ email, src: "LP", time: unixseconds() })
+        .then(({ id }) =>
+          sendMail.send({
+            to: email,
+            from: process.env.notificationEmailSender,
+            subject: `Ministry Of Pup: You are now Subscribed to cuteness!`,
+            html: emailString(id),
+          })
+        );
+    else if (snapshot.docs[0].data().cancelled)
+      await fs
+        .collection("newsletter")
+        .doc(snapshot.docs[0].id)
+        .update({
+          cancelled: require("firebase-admin").firestore.FieldValue.delete(),
+        });
+
+    res.status(200).json({ ok: true });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ ok: false, error: `Failed to subscribe\n${error.message}` });
+  }
+});
 
 /**
  * Cancel a subscription but keeping data for future use/analytics, just adding a cancelled field
@@ -66,7 +94,7 @@ router.post(
       .doc(req.params.newsletterDocID)
       .update({ cancelled: true })
       .then(() => res.status(200).json({ ok: true }))
-      .catch(() => res.status(400).json({ ok: true, error: "Cancel failed" }))
+      .catch(() => res.status(400).json({ ok: false, error: "Cancel failed" }))
   )
 );
 
