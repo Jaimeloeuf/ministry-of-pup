b8baf1c7a2e1facfb296afcd7bdc14cb283894ca
Jaime <jaimeloeuf@gmail.com>
2021-11-03 15:27:25 +0800

    Add QR code generation for booking links:     So that admin can share the QR code or scan directly.

--- admin/src/views/BookingLinks.vue
+++ admin/src/views/BookingLinks.vue
@@ -8,6 +8,13 @@
       <p class="subtitle has-text-success" v-if="showCopied">Copied!</p>
     </div>
 
+    <div class="column is-narrow">
+      <label class="checkbox">
+        <input type="checkbox" v-model="showQRCode" />
+        Show QR
+      </label>
+    </div>
+
     <div class="column is-full">
       <hr class="my-0" style="background-color: #dedede" />
     </div>
@@ -27,6 +34,25 @@
         {{ link.text }}
       </button>
     </div>
+
+    <div class="modal" :class="{ 'is-active': showModal }">
+      <!-- Modal can be closed by clicking any part of the modal background -->
+      <div class="modal-background" @click="showModal = false"></div>
+
+      <!-- Modal can be closed by clicking any part of the modal image -->
+      <div class="modal-content" @click="showModal = false">
+        <span class="image is-square">
+          <img :src="imageDataURI" />
+        </span>
+      </div>
+
+      <!-- Modal can be closed by clicking the top right X -->
+      <button
+        class="modal-close is-large"
+        aria-label="close"
+        @click="showModal = false"
+      />
+    </div>
   </div>
 </template>
 
@@ -37,17 +63,38 @@ export default {
   methods: {
     // Cannot be used directly in the template as navigator is not available in that context
     copy(link) {
-      navigator.clipboard.writeText(link).then(() => {
-        this.showCopied = true;
+      if (this.showQRCode) this.showQR(link);
+      else
+        navigator.clipboard.writeText(link).then(() => {
+          this.showCopied = true;
 
-        setTimeout(() => (this.showCopied = false), 1100);
-      });
+          setTimeout(() => (this.showCopied = false), 1100);
+        });
+    },
+
+    async showQR(link) {
+      const QRCode = await import("qrcode");
+
+      // Generate the QR code image data URL and set onto component data value
+      this.imageDataURI = await QRCode.toDataURL(
+        link,
+
+        // Use high error resistance rate of ~ 30%
+        { errorCorrectionLevel: "H" }
+      );
+
+      // Open up modal to show the QR Code image
+      this.showModal = true;
     },
   },
 
   data() {
     return {
       showCopied: false,
+
+      showQRCode: false,
+      showModal: false,
+      imageDataURI: undefined,
 
       // Might load this from DB...?
       links: [
