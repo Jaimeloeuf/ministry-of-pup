be244bb85109ab82813ed6774dec508a00522dd6
Jaime <jaimeloeuf@gmail.com>
2021-11-27 16:11:47 +0800

    Initial Commit.

---
+++ appointment-src/index.js
@@ -0,0 +1,11 @@
+export default {
+  LA: "Landing Page",
+  IG: "Instagram",
+  FB: "Facebook",
+  WA: "Whatsapp",
+  GG: "Google",
+  WC: "WeChat",
+  WI: "Walk In",
+  OT: "Others",
+  UN: "Undefined",
+};
