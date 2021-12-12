fe3d33895e2691bcb575ae99d7a55b2f1ec3a23d
Jaime <jaimeloeuf@gmail.com>
2021-12-13 03:52:57 +0800

    Add get receipt by number API and update dep: 1.  Add API to get receipt by receipt number, and refactor out common     code with get receipt by receipt ID. 2.  Update receipt lib to support optional customer details in receipts.

--- api/src/routes/receipt.js
+++ api/src/routes/receipt.js
@@ -10,10 +10,21 @@ const router = express.Router();
 const fs = require("../utils/fs");
 const { asyncWrap } = require("express-error-middlewares");
 
+/** Set content headers, generate receipt, pipe it back to client and end connection */
+async function generateAndSendReceipt(res, receiptData) {
+  // Set content header first before generating receipt sending it to the client
+  res.setHeader("Content-Type", "application/pdf");
+
+  // Generate receipt, pipe it back to the client and end the connection
+  return require("../utils/receipt")
+    .generateReceiptString(receiptData, res)
+    .then(res.status(200).end);
+}
+
 /**
  * API to request server to generate the receipt for the given receipt ID
  * @name GET /receipt/:receiptID
- * @returns Sucess indicator and receipt
+ * @returns Receipt
  */
 router.get(
   "/:receiptID",
@@ -25,13 +36,28 @@ router.get(
     if (!snapshot.exists)
       return res.status(404).json({ error: "Receipt not found" });
 
-    // Set content header first before generating receipt sending it to the client
-    res.setHeader("Content-Type", "application/pdf");
+    generateAndSendReceipt(res, snapshot.data());
+  })
+);
 
-    // Generate receipt, pipe it back to the client and end the connection
-    await require("../utils/receipt")
-      .generateReceiptString(snapshot.data(), res)
-      .then(res.status(200).end);
+/**
+ * API to request server to generate the receipt for the given receipt number
+ * @name GET /receipt/number/:receiptNumber
+ * @returns Receipt
+ */
+router.get(
+  "/number/:receiptNumber",
+  asyncWrap(async (req, res) => {
+    const { receiptNumber } = req.params;
+
+    const snapshot = await fs
+      .collection("receipts")
+      .where("receiptNumber", "==", receiptNumber)
+      .get();
+    if (snapshot.empty)
+      return res.status(404).json({ error: "Receipt not found" });
+
+    generateAndSendReceipt(res, snapshot.docs[0].data());
   })
 );
 
