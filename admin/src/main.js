7f5ce4f87c9324145b9b391df009679e73ef66b0
Jaime <jaimeloeuf@gmail.com>
2021-12-14 23:57:49 +0800

    Remove unused redirect logic.

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,15 +1,3 @@
-/* For sites that can be accessed via multiple domains */
-// Function to redirect user to the preferred domain instead of the default firebase hosting domains if they land there.
-// IIFE to prevent polluting the global namespace accidentally
-(function () {
-  const location = window.location.hostname;
-  console.log(`Window location: ${location}`);
-
-  // Add links to redirect as needed
-  // if (location === "example.com" || location === "example2.com")
-  //   window.location.replace("https://yourdomain.com");
-})();
-
 import { auth, onAuthStateChanged } from "./firebase";
 
 // Import first before this is used elsewhere to set the baseUrl
