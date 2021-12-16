bb6f257125918b2bc46d9928311335f0f9db9378
Jaime <jaimeloeuf@gmail.com>
2021-12-16 15:05:18 +0800

    Add header to set file name for receipt:     Add header for receipt API to set a default file name to use when     user tries to save the file.

--- api/src/routes/receipt.js
+++ api/src/routes/receipt.js
@@ -15,6 +15,20 @@ async function generateAndSendReceipt(res, receiptData) {
   // Set content header first before generating receipt sending it to the client
   res.setHeader("Content-Type", "application/pdf");
 
+  // No matter what the browser uses the last part of the URI rather than the filename as the page title...
+  // So 1 way is to always redirect to /receipt/number/:receiptNumber to have it named correctly
+  // The second way is just ignore it, then when user downloads the file, default that filename to receiptNumber
+  res.setHeader(
+    "Content-disposition",
+    `inline; filename=${receiptData.receiptNumber}.pdf`
+  );
+
+  // Alternative header to use to make browser auto trigger save file action when link is opened
+  // res.setHeader(
+  //   "Content-disposition",
+  //   `attachment; filename=${receiptData.receiptNumber}.pdf`
+  // );
+
   // Generate receipt, pipe it back to the client and end the connection
   return require("../utils/receipt")
     .generateReceiptString(receiptData, res)
