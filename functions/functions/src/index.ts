702ef46e597b1b851e8df01ddfd7547f5c05ac53
Jaime <jaimeloeuf@gmail.com>
2022-03-17 22:52:49 +0800

    Add access control headers for cloud function:     Add Access Control HTTP headers for cloud function so that the landing     page can access that API from a different domain.

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,3 +1,8 @@
+/*
+  Might migrate to gen2 cloud functions in the future
+  https://cloud.google.com/functions/docs/2nd-gen/2nd-gen-differences
+*/
+
 import * as functions from "firebase-functions";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
@@ -5,14 +10,18 @@ import { getFirestore } from "firebase-admin/firestore";
 initializeApp();
 
 export const getDogs = functions
+  /* Run function in Singapore only */
   .region("asia-southeast1")
-  .https.onRequest(
-    (_, r) =>
-      getFirestore()
-        .collection("dogs")
-        .where("show", "==", true)
-        .get()
-        .then(({ docs }) => docs.map((doc) => doc.data()))
-        .then((dogs) => r.status(200).json({ dogs }))
-        .catch((_: Error) => r.status(500).json({ error: "Failed" })) as any
-  );
+  .https.onRequest((_, r) => {
+    r.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    });
+    getFirestore()
+      .collection("dogs")
+      .where("show", "==", true)
+      .get()
+      .then(({ docs }) => docs.map((doc) => doc.data()))
+      .then((dogs) => r.status(200).json({ dogs }))
+      .catch((_: Error) => r.status(500).json({ error: "Failed" }));
+  });
