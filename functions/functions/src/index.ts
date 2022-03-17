34f0d4b5510c302818f8c5d1705c0a98635cf410
Jaime <jaimeloeuf@gmail.com>
2022-03-18 02:00:53 +0800

    Modifiy cloud function's runtime constraints.

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -12,6 +12,17 @@ initializeApp();
 export const getDogs = functions
   /* Run function in Singapore only */
   .region("asia-southeast1")
+  /* Modify the runtime constraints and behaviour of the cloud function */
+  .runWith({
+    // Default timeout of firebase function is 60 seconds
+    // However this function does not need that long since it is just a FS call
+    // Thus reducing timeout to ensure that it will not accidentally run too long
+    timeoutSeconds: 10,
+
+    // To prevent cost from exceeding
+    // However this becomes vulnerable to DDoS, but at least DB wont get hit with recaptcha protection
+    maxInstances: 20,
+  })
   .https.onRequest((_, r) => {
     r.set({
       "Access-Control-Allow-Origin": "*",
