5fa9a4ccaa1bbfc1b71bad92c2e5b7b3f6948351
Jaime <jaimeloeuf@gmail.com>
2022-05-12 01:16:11 +0800

    Complete basic Go API service:     Re-implement cloud function API server in go.

--- gapi/src/main.go
+++ gapi/src/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
+
+	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/iterator"
 )
 
 func main() {
+	// ================== Initialize the firebase stuff ==================
 
 	// Create firebase app
 	app, err := firebase.NewApp(context.Background(), nil)
@@ -23,6 +26,26 @@ func main() {
 		log.Fatalf("Error initializing firestore client: %v\n", err)
 	}
 
+	// ================== Initialize the gin HTTP server ==================
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		// todo verify recaptcha
+
+		c.JSON(200, gin.H{"dogs": getDogs(client)})
+	})
+
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run()
+}
+
+// Empty map type for dog document data
+type keyvalue map[string]interface{}
+
+// Function to read all dogs document to show from firestore
+func getDogs(client *firestore.Client) []keyvalue {
+	var dogs []keyvalue
+
 	// Create iterator for the query
 	iter := client.Collection("dogs").Where("show", "==", true).Documents(context.Background())
 	defer iter.Stop()
@@ -35,7 +58,8 @@ func main() {
 			log.Fatalf("Error reading document: %v\n", err)
 		}
 
-		fmt.Println(doc.Data())
+		dogs = append(dogs, doc.Data())
 	}
 
+	return dogs
 }
