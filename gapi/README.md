9cfe266ad2965e28ee6e205925b3e8e688bfa9e9
Jaime <jaimeloeuf@gmail.com>
2022-05-11 23:49:24 +0800

    Scaffold Go API subrepo:     Experimental Go API server to make landing page get dogs API call     faster by using Go instead of node js to reduce cold start time.

---
+++ gapi/README.md
@@ -0,0 +1,2 @@
+# gapi
+API for landing page written in Go to be super fast.
---
+++ gapi/src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
+)
+
+func main() {
+
+	// Create firebase app
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error initializing app: %v\n", err)
+	}
+
+	// Create firestore client
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalf("Error initializing firestore client: %v\n", err)
+	}
+
+	// Create iterator for the query
+	iter := client.Collection("dogs").Where("show", "==", true).Documents(context.Background())
+	defer iter.Stop()
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error reading document: %v\n", err)
+		}
+
+		fmt.Println(doc.Data())
+	}
+
+}
