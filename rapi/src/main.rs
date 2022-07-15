1ea7cd3eb288832871d06b8d5515edc346c83b6a
Jaime <jaimeloeuf@gmail.com>
2022-07-16 01:11:24 +0800

    Implement basic routes in rapi.

--- rapi/src/main.rs
+++ rapi/src/main.rs
@@ -1,11 +1,25 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "'Ministry Of Pup' API Service"
+}
+
+// Use this for both Kubernetes Liveness and Readiness probe.
+#[get("/health")]
+fn health() -> &'static str {
+    "Ok"
+}
+
+// Get API version, aka the latest git commit's hash injected by the docker build process
+#[get("/version")]
+fn version() -> &'static str {
+    "process.env.version" // @todo
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, health, version])
 }
