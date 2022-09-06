04391301acddf3f18de0041e22cf5299affa62df
Jaime <jaimeloeuf@gmail.com>
2022-09-06 15:51:43 +0800

    Update simpler-fetch library and code.

--- booking/src/store.js
+++ booking/src/store.js
@@ -2,10 +2,11 @@ import { createStore } from "vuex";
 import { oof } from "simpler-fetch";
 
 // Set baseUrl before using in actions
-oof._baseUrl =
+oof.setBaseURL(
   process.env.NODE_ENV === "production"
     ? "https://api.ministryofpup.com"
-    : "http://localhost:3000";
+    : "http://localhost:3000"
+);
 
 const getRecaptchaToken = async (action) =>
   new Promise((resolve, reject) =>
@@ -66,7 +67,7 @@ export default createStore({
     async loadDates({ commit, dispatch }, after) {
       const token = await getRecaptchaToken("getAvailableDates");
 
-      const res = await oof
+      const { res, err } = await oof
         .GET(
           after
             ? `/appointment/available/date?after=${after}`
@@ -75,8 +76,14 @@ export default createStore({
         .header({ "x-recaptcha-token": token })
         .runJSON();
 
-      // If the API call failed, recursively dispatch itself again if user wants to retry,
-      // And always make sure that this method call ends right here by putting it in a return expression
+      // If the API call failed for whatever reason, recursively dispatch itself again if user wants to retry,
+      // And always make sure that this method call ends right here by putting it in a return expression.
+      if (err)
+        return (
+          confirm(`Error: \n${err}\n\nTry again?`) &&
+          dispatch("loadDates", after)
+        );
+
       if (!res.ok)
         return (
           confirm(`Error: \n${res.error}\n\nTry again?`) &&
@@ -90,85 +97,76 @@ export default createStore({
     },
 
     async book({ commit, dispatch, state }) {
-      try {
-        const token = await getRecaptchaToken("bookAppointment");
+      const token = await getRecaptchaToken("bookAppointment");
 
-        const res = await oof
-          .POST("/appointment/book")
-          .header({ "x-recaptcha-token": token })
-          .data({
-            time: state.selectedTimeslot,
-            src: state.src,
+      const { res, err } = await oof
+        .POST("/appointment/book")
+        .header({ "x-recaptcha-token": token })
+        .bodyJSON({
+          time: state.selectedTimeslot,
+          src: state.src,
 
-            // Add in these fields to submit
-            // fname / lname / number / email / preference / referralCode
-            ...state.details,
-          })
-          .runJSON();
+          // Add in these fields to submit
+          // fname / lname / number / email / preference / referralCode
+          ...state.details,
+        })
+        .runJSON();
 
-        // If the API call failed, recursively dispatch itself again if user wants to retry,
-        // And always make sure that this method call ends right here by putting it in a return expression
-        if (!res.ok)
-          return (
-            confirm(`Error: \n${res.error}\n\nTry again?`) && dispatch("book")
-          );
+      // If the API call failed for whatever reason, recursively dispatch itself again if user wants to retry,
+      // And always make sure that this method call ends right here by putting it in a return expression.
+      if (err)
+        return confirm(`Error: \n${err}\n\nTry again?`) && dispatch("book");
 
-        commit("setter", ["appointmentID", res.appointmentID]);
-
-        // Return true to indicate that appointment was successfully booked
-        return true;
-      } catch (error) {
-        // For errors that cause API call itself to throw
-        console.error(error);
-
-        // If the API call failed, recursively dispatch itself again if user wants to retry,
-        // And always make sure that this method call ends right here by putting it in a return expression
+      if (!res.ok)
         return (
-          confirm(`Error: \n${error.message}\n\nTry again?`) && dispatch("book")
+          confirm(`Error: \n${res.error}\n\nTry again?`) && dispatch("book")
         );
-      }
+
+      commit("setter", ["appointmentID", res.appointmentID]);
+
+      // Return true to indicate that appointment was successfully booked
+      return true;
     },
 
-    async reschedule({ commit, dispatch, state }) {
-      try {
-        const token = await getRecaptchaToken("rescheduleAppointment");
+    async reschedule({ dispatch, state }) {
+      const token = await getRecaptchaToken("rescheduleAppointment");
 
-        const res = await oof
-          .POST(`/appointment/reschedule/${state.appointmentID}`)
-          .header({ "x-recaptcha-token": token })
-          .data({ time: state.selectedTimeslot })
-          .runJSON();
+      const { res, err } = await oof
+        .POST(`/appointment/reschedule/${state.appointmentID}`)
+        .header({ "x-recaptcha-token": token })
+        .bodyJSON({ time: state.selectedTimeslot })
+        .runJSON();
 
-        // If the API call failed, recursively dispatch itself again if user wants to retry,
-        // And always make sure that this method call ends right here by putting it in a return expression
-        if (!res.ok)
-          return (
-            confirm(`Error: \n${res.error}\n\nTry again?`) &&
-            dispatch("reschedule")
-          );
-      } catch (error) {
-        // For errors that cause API call itself to throw
-        console.error(error);
-
-        // If the API call failed, recursively dispatch itself again if user wants to retry,
-        // And always make sure that this method call ends right here by putting it in a return expression
+      // If the API call failed for whatever reason, recursively dispatch itself again if user wants to retry,
+      // And always make sure that this method call ends right here by putting it in a return expression.
+      if (err)
         return (
-          confirm(`Error: \n${error.message}\n\nTry again?`) &&
+          confirm(`Error: \n${err}\n\nTry again?`) && dispatch("reschedule")
+        );
+
+      if (!res.ok)
+        return (
+          confirm(`Error: \n${res.error}\n\nTry again?`) &&
           dispatch("reschedule")
         );
-      }
     },
 
     async cancel({ dispatch }, appointmentID) {
       const token = await getRecaptchaToken("cancelAppointment");
 
-      const res = await oof
+      const { res, err } = await oof
         .POST(`/appointment/cancel/${appointmentID}`)
         .header({ "x-recaptcha-token": token })
         .runJSON();
 
-      // If the API call failed, recursively dispatch itself again if user wants to retry,
-      // And always make sure that this method call ends right here by putting it in a return expression
+      // If the API call failed for whatever reason, recursively dispatch itself again if user wants to retry,
+      // And always make sure that this method call ends right here by putting it in a return expression.
+      if (err)
+        return (
+          confirm(`Error: \n${err}\n\nTry again?`) &&
+          dispatch("cancel", appointmentID)
+        );
+
       if (!res.ok)
         return (
           confirm(`Error: \n${res.error}\n\nTry again?`) &&
